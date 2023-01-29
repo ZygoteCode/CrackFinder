bf3b0f7f75ec12c2d44bccff9992da293045b9ce
Gabriele Bologna <bolognagabriele2003@gmail.com>
2023-01-29 22:59:28 +0100

    First commit

---
+++ MainForm.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Forms;
+using System.Diagnostics;
+using System.Web;
+
+public partial class MainForm : Form
+{
+    public MainForm()
+    {
+        InitializeComponent();
+    }
+
+    private void button1_Click(object sender, System.EventArgs e)
+    {
+        string newStr = HttpUtility.UrlEncode(textBox1.Text);
+        Process.Start("https://filecr.com/?s=" + newStr);
+        Process.Start("https://getintopc.com/?s=" + newStr);
+    }
+
+    private void textBox1_KeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.KeyCode == Keys.Enter)
+        {
+            button1.PerformClick();
+        }
+    }
+}
